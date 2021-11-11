8c4b8e1e7102b0e4c1140c3b745c53614e981f55
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2021-11-11 13:26:05 -0500

    ah, more decoding

--- index.js
+++ index.js
@@ -191,10 +191,11 @@ async function main() {
    * Log in to Docker
    */
   const getAuthCmd = new GetAuthorizationTokenCommand({});
-  let ecrAuthToken;
+  let ecrUser, ecrPass;
   try {
     const resp = await ecrClient.send(getAuthCmd);
-    ecrAuthToken = resp.authorizationData[0].authorizationToken;
+    const ecrAuthToken = resp.authorizationData[0].authorizationToken;
+    console.log(Buffer.from(ecrAuthToken, "base64").toString("utf8"));
   } catch (e) {
     core.error(e);
     core.error("Unable to obtain ECR password");
@@ -202,17 +203,17 @@ async function main() {
   }
 
   // Mask the token in logs
-  core.setSecret(ecrAuthToken);
+  // core.setSecret(ecrAuthToken);
 
-  const { stdout } = await execFile("docker", [
-    "login",
-    "--username",
-    "AWS",
-    "--password",
-    ecrAuthToken,
-    inputs.ecrURI,
-  ]);
-  console.log(stdout);
+  // const { stdout } = await execFile("docker", [
+  //   "login",
+  //   "--username",
+  //   "AWS",
+  //   "--password",
+  //   ecrAuthToken,
+  //   inputs.ecrURI,
+  // ]);
+  // console.log(stdout);
 }
 
 async function assertECRRepo(client, repository) {
