63a4b37252d043e0babd4304a3093d70fdd44ecf
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2021-12-02 16:13:12 -0500

    where does npm live

--- setup.js
+++ setup.js
@@ -4,6 +4,9 @@ const { promisify } = require("util");
 const execFile = promisify(child_process.execFile);
 
 (async () => {
+  const { stdout } = await execFile("which npm");
+  console.log(stdout);
+
   await execFile("/usr/bin/npm", ["install"], {
     env: { NODE_ENV: "production" },
     cwd: process.env.GITHUB_ACTION_PATH,
