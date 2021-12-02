28db88bfc75f5f2dcb30b40cab6230606eb00c04
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2021-12-02 16:15:24 -0500

    maybe simplify

--- setup.js
+++ setup.js
@@ -1,14 +1,5 @@
 const child_process = require("child_process");
-const { promisify } = require("util");
 
-const execFile = promisify(child_process.execFile);
-
-(async () => {
-  const { stdout } = await execFile("which npm");
-  console.log(stdout);
-
-  await execFile("/usr/bin/npm", ["install"], {
-    env: { NODE_ENV: "production" },
-    cwd: process.env.GITHUB_ACTION_PATH,
-  });
-})();
+child_process.exec("npm install --production", {
+  cwd: process.env.GITHUB_ACTION_PATH,
+});
